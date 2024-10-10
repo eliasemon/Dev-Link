766cb687aa818a60075666a63a5abfbbe2f0ad0e
Elias Emon <eliasemon0025@gmail.com>
2024-10-10 08:59:01 +0600

    feat: devlink model added

---
+++ backend/src/db/models/devLinks.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, Document, Types } from 'mongoose';
+
+// Define an interface representing a link object in DevLinks.
+interface ILink {
+  platform: string;
+  link: string;
+}
+
+// Define an interface for DevLinks which extends the Mongoose Document.
+export interface IDevLinks extends Document {
+  userId: Types.ObjectId; // Reference to the User schema
+  links?: ILink[]; // Array of link objects
+}
+
+// Create a Schema corresponding to the document interface.
+const devLinksSchema: Schema<IDevLinks> = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    links: [
+      {
+        platform: {
+          type: String,
+        },
+        link: {
+          type: String,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+// Create the Mongoose model.
+const DevLinks = mongoose.model<IDevLinks>('DevLinks', devLinksSchema);
+
+export default DevLinks;
